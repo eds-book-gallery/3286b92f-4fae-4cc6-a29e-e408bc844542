a41f3fe2ec6091abb69e7d83bec71e4338fcb72a
Garima <garimam@umich.edu>
2023-05-21 17:25:05 +0000

    Added Garima's notebook again

--- infrastructure/downloader.sh
+++ infrastructure/downloader.sh
@@ -1,10 +1,9 @@
+
+
 # download source data to raw schema
-mkdir ../data/
-mkdir ../data/raw/
 cd ../data/raw/
 
 zenodo_get 10.5281/zenodo.7919172
-zenodo_get 10.5281/zenodo.7953838
 
 # unzip data
 for f in *.gz
