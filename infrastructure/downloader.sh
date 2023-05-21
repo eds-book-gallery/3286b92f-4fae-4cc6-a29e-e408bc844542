43caf0fc5f6918b433c8f4869e7f246120aee8c4
jorge <jorge_eduardo2894@hotmail.com>
2023-05-21 13:23:47 -0700

    Fixed downloader

--- infrastructure/downloader.sh
+++ infrastructure/downloader.sh
@@ -1,27 +1,30 @@
 # download source data to raw schema
-#mkdir ../data/
-#mkdir ../data/raw/
-#cd ../data/raw/
+mkdir -p ../data/raw/
+cd ../data/raw/
 
-#zenodo_get 10.5281/zenodo.7919172
+zenodo_get 10.5281/zenodo.7919172
 
 # unzip data
-#for f in *.gz
-#    do echo "Processing $f file..." 
-#    gzip -d "$f"
-#done
+for f in *.gz
+    do echo "Processing $f file..." 
+    gzip -d "$f"
+done
 
 #Input output data
-cd ../outputs/
+cd ../../outputs/
 zenodo_get 10.5281/zenodo.7954232
 
 # unzip data
 for f in *.gz
     do echo "Processing $f file..." 
-    tar -xvf -d "$f"
+    tar -xzf "$f"
+    rm "$f"
 done
 
-mv MODELS ../outputs/models/
-mv predictions ../outputs/predictions/
+mv MODELS/* ../outputs/models/ && rm -rf MODELS
+mv ITERATED_PREDICTION_ARRAYS/* ../outputs/predictions/ && rm -rf ITERATED_PREDICTION_ARRAYS
+
 mkdir ../outputs/interim
-mv trunc_io_arrays/* ../outputs/interim/
+mv trunc_io_arrays/* ../outputs/interim/ && rm trunc_io_arrays
+
+rm md5sums.txt
