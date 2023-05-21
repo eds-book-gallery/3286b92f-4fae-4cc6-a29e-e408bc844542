eb52fec961a113fa9e2586e131e9c192b32c9a3e
Garima <garimam@umich.edu>
2023-05-21 19:18:06 +0000

    downloader

--- infrastructure/downloader.sh
+++ infrastructure/downloader.sh
@@ -1,15 +1,27 @@
-
-
 # download source data to raw schema
-cd ../data/raw/
+#mkdir ../data/
+#mkdir ../data/raw/
+#cd ../data/raw/
 
-zenodo_get 10.5281/zenodo.7919172
+#zenodo_get 10.5281/zenodo.7919172
+
+# unzip data
+#for f in *.gz
+#    do echo "Processing $f file..." 
+#    gzip -d "$f"
+#done
+
+#Input output data
+cd ../outputs/
+zenodo_get 10.5281/zenodo.7954232
 
 # unzip data
 for f in *.gz
     do echo "Processing $f file..." 
-    gzip -d "$f"
+    tar -xvf -d "$f"
 done
 
-
-
+mv MODELS ../outputs/models/
+mv predictions ../outputs/predictions/
+mkdir ../outputs/interim
+mv trunc_io_arrays/* ../outputs/interim/
